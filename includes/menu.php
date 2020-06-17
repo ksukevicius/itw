82be15e19f6ea5c687ac4a669be0a1b3bc0dc643
Kestas <k.sukevicius@gmail.com>
2020-06-17 16:40:17 +0300

    Initial commit

---
+++ includes/menu.php
@@ -0,0 +1,14 @@
+ <!-- A grey horizontal navbar that becomes vertical on small screens -->
+<nav class="navbar navbar-expand-sm bg-light">
+
+	<!-- Links -->
+	<ul class="navbar-nav">
+		<li class="nav-item">
+		  <a class="nav-link" href="index.php">Home</a>
+		</li>
+		<li class="nav-item">
+		  <a class="nav-link" href="position_new.php">New Position</a>
+		</li>
+	</ul>
+
+</nav> 
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>IT World</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<?php
+	include 'includes/menu.php';
+	?>
+
+	<div>the purpose of this project it to gather much information about IT WORLD</div>
+</body>
+</html>
