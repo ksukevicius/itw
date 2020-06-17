19a7389830445a7ed6d06e9ca738bc8d13d6660b
Kestas <k.sukevicius@gmail.com>
2020-06-17 18:06:14 +0300

    added config folder and connection to db and implemented form to enter new position

--- position_new.php
+++ position_new.php
@@ -1,3 +1,6 @@
+<?php
+require 'config/connection.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,10 +17,50 @@
 	include 'includes/menu.php';
 	?>
 	<div class="container">
-		<div class="jumbotron my-3">there I want to implement form to enter IT job titles / positions</div>
+		<div class="jumbotron my-3">
+			<pre>
+			there I want to implement form to enter IT job titles / positions
+			using Oracle 12c database
+			</pre>
+		</div>
 		
+		<form method="post" action="position_new.php">
+			<div class="form-group">
+				<label for="position">Position:</label>
+				<input type="text" class="form-control" id="position" name="position" placeholder="Enter position (job title)" >
+			</div>	
+			<button type="submit" class="btn btn-primary">Submit</button>
+		</form>
+
+		<?php
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			echo "<hr>";
+			echo "Form was submitted";
+			echo "<hr>";
+			//htmlentities - need to check when it's needed
+			echo "There is the information in global variable \$_POST";
+			echo "<hr>";
+			echo '<pre>'; print_r($_POST); echo '</pre>';
+			
+			// get POST values, probably need to sanitize (clean up, check what user entered)
+			$position = $_POST['position'];
+			// sql statement to execute
+			$sql = "INSERT INTO C##Kestas.POSITIONS2(position) VALUES(:position)";
+			// Prepares an Oracle statement for execution
+			// Returns a statement handle on success, or FALSE on error. 
+			$stid = oci_parse($conn, $sql);
+			oci_bind_by_name($stid, ':POSITION', $position);
+			//Returns TRUE on success or FALSE on failure. 
+			$res = oci_execute($stid);
+			oci_free_statement($stid);			
+		}
+		?>		
 		
 	</div>	
 	
 </body>
 </html>
+
+<?php
+oci_close($conn)
+?>
