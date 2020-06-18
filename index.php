6cf96bf96119b055daa56be528cf5d4016ea6ea7
Kestas <k.sukevicius@gmail.com>
2020-06-18 18:07:40 +0300

    added some  info about HTF.ESCAPE function

--- index.php
+++ index.php
@@ -17,6 +17,15 @@
 	
 		<div class="jumbotron my-3">the purpose of this project it to gather much information about IT WORLD</div>
 	
+		<div class="border border-primary my-3 p-2">
+			<h2>HTF.ESCAPE_SC</h2>
+			<p>This replaces characters that have special meaning in HTML with their escape sequences. The following characters are converted:</p>
+			<font size="2">&amp; to &amp;amp;         " to &amp;quot;       &lt; to &amp;lt;   &gt; to &amp;gt; </font>
+			<p>situation BEFOR and AFTER in php web site and oracle database</p>
+			<img src="img/html_special.jpg" />
+			<img src="img/html_special_oracle.jpg" />
+		</div>
+	
 		<a data-fancybox data-ratio="2" href="https://www.youtube.com/watch?v=e-5obm1G_FY" >
 			<p>Learning Functional Programming with JavaScript - Anjana Vakil - JSUnconf</p>
 		</a>
