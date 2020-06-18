f0ea292ffdc80462f8dfcef0de0095f62d9811e7
Kestas <k.sukevicius@gmail.com>
2020-06-18 18:08:27 +0300

    updated menu

--- includes/menu.php
+++ includes/menu.php
@@ -10,6 +10,9 @@
 		  <a class="nav-link" href="position_new.php">New Position</a>
 		</li>
 		<li class="nav-item">
+		  <a class="nav-link" href="position_new_sp.php">New Position SP</a>
+		</li>		
+		<li class="nav-item">
 		  <a class="nav-link" href="position_all.php">All Positions</a>
 		</li>
 		
