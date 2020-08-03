9e5ba5057da31a60f06e7e0479a912c2cf093314
Kestas <k.sukevicius@gmail.com>
2020-08-03 22:07:34 +0300

    new info

--- linux.php
+++ linux.php
@@ -17,7 +17,43 @@
 	
 	<div class="container">
 	
-		<div class="jumbotron my-3">There I will put my notes about Linux</div>
+		<div class="jumbotron my-3">
+			<div class="row">
+				<div class="col-2">	
+					<img class="mx-3" style="width:100px; float:left;" src="img\logos\linux.png" />
+				</div>
+				<div class="col-10">
+					<h3>
+						There I will put my notes about Linux
+					</h3>
+				</div>
+			</div>			
+		</div>
+
+		<div class="mypost row border border-primary my-3 p-2">
+			<div class="col-6">
+				<p class="mynote">
+					I have my Laravel/Homestead vm up and running and there is Apache web server on this machine, but looks like this service is not setup to start automatically. 
+					Need to take a look how to configure service to start automatically.
+				</p>
+				<p class="mynote">
+					The second way to check service status<kbd class="mykbd">systemctl status apache2</kbd>and the result is the same apache2 service is not started. 
+					<img src="img\linux\apache_service2.jpg" class = "img-responsive" width = "100%" />
+					And I don't want to do it manually every time I start my VM.
+				</p>
+				<p class="mynote"> 
+					is it that simple... ?
+					<kbd class="mykbd">sudo systemctl enable apache2</kbd> 
+					just one command
+					<img src="img\linux\apache2_started.jpg" class = "img-responsive" width = "100%" />
+				</p>
+				<p><a href="https://linuxconfig.org/how-to-start-service-on-boot-on-ubuntu-20-04" target="_blank">More info there</a></p>
+			</div>
+			<div class="col-6">
+				<p class="mynote"><kbd>service --status-all</kbd></p>
+				<img src="img\linux\apache_service.jpg" class = "img-responsive" width = "100%" />
+			</div>
+		</div>
 
 		<div class="mypost row border border-primary my-3 p-2">
 			<div class="col-6">
